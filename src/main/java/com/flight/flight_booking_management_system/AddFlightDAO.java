79f714b9cc75a88b54366cbf38301198a60862f8
Ak-Rajak <atulkumar.coderguys@gmail.com>
2024-08-25 13:04:33 +0530

    Implemented the Modify Flight into features in the HomeAdmin i.e 1)modifyflight 2)modifyflightservlet 3)AddflightDao files to update the files

--- src/main/java/com/flight/flight_booking_management_system/AddFlightDAO.java
+++ src/main/java/com/flight/flight_booking_management_system/AddFlightDAO.java
@@ -45,4 +45,23 @@ public class AddFlightDAO {
     }
 
     // Additional methods for update, delete, and fetch flights can be added here
+    public boolean modifyFlight(ModifyFlight flight) {
+        String sql = "UPDATE flights SET airline = ?, departure_city = ?, arrival_city = ?, departure_time = ?, arrival_time = ?, price = ? WHERE flight_number = ?";
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, flight.getAirline());
+            preparedStatement.setString(2, flight.getDepartureCity());
+            preparedStatement.setString(3, flight.getArrivalCity());
+            preparedStatement.setString(4, flight.getDepartureTime());
+            preparedStatement.setString(5, flight.getArrivalTime());
+            preparedStatement.setDouble(6, flight.getPrice());
+            preparedStatement.setString(7, flight.getFlightNumber());
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+            return rowsUpdated > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
