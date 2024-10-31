c8ee48f242c096696898997c94de310f07fb8028
kalinga kishore parida <kalingakishore2003@gmail.com>
2024-10-31 20:46:30 +0530

    added the departure time and arraival time getrequest parameter

--- src/main/java/com/flight/flight_booking_management_system/AdminFunctions/DeleteFlight.java
+++ src/main/java/com/flight/flight_booking_management_system/AdminFunctions/DeleteFlight.java
@@ -2,6 +2,30 @@ package com.flight.flight_booking_management_system.AdminFunctions;
 
 public class DeleteFlight {
     private String flightNumber;
+    private String departureTime;
+    private String arrivalTime;
+
+    public DeleteFlight(String flightNumber, String departureTime, String arrivalTime) {
+        this.flightNumber = flightNumber;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
 
     // Constructor
     public DeleteFlight(String flightNumber) {
